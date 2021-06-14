2ab370bdd62060b4849277f081b8b926a881e7bd
Eedal <elkindearmas2015@gmail.com>
2021-06-14 11:49:47 -0500

    agrego la url my-url

--- oxxo/urls.py
+++ oxxo/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from productos.views import ChelaViewSet
-
+from productos.views import index
 router = DefaultRouter()
 router.register(r'productos', ChelaViewSet)
 
@@ -11,4 +11,5 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('my-url/', index)
 ]
--- productos/views.py
+++ productos/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Chela
 from .serializers import ChelaSerializer
-
+from django.http import HttpResponse
 class ChelaViewSet(viewsets.ModelViewSet):
     serializer_class = ChelaSerializer
     queryset = Chela.objects.all()
 
     
+def index(request):
+    return HttpResponse("index")
